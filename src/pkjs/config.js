84acd956547ff387f37a47890d3408c51415cc10
Baptiste Candellier <baptiste@candellier.me>
2026-04-03 23:10:10 +0200

    feat: use link that works instead of broken button

--- src/pkjs/config.js
+++ src/pkjs/config.js
@@ -11,10 +11,8 @@ module.exports = [
         "defaultValue": "Credentials"
       },
       {
-        "type": "button",
-        "id": "btnConnectTwitch",
-        "primary": true,
-        "defaultValue": "Connect with Twitch"
+        "type": "text",
+        "defaultValue": "Click <a href=\"https://id.twitch.tv/oauth2/authorize?client_id=53p2uwy4r832kgbxqs66g4i3idtjso&force_verify=true&redirect_uri=https%3A%2F%2Foutadoc.github.io%2Fpebble-twitch%2Fcallback.html&response_type=token&scope=user%3Aread%3Afollows\">here</a> to authorize your Twitch account to access the app, then paste your token in field below."
       },
       {
         "type": "input",
--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,8 +1,7 @@
 var Clay = require('@rebble/clay');
 
 var clayConfig = require('./config');
-var clayCustom = require('./clay-custom');
-var clay = new Clay(clayConfig, clayCustom);
+var clay = new Clay(clayConfig);
 
 var MAX_STREAMS = 20;
 var TWITCH_API = 'https://api.twitch.tv/helix';
